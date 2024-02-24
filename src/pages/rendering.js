85cc75f534a956ab9410b0abc32051a81423d58d
David Sokol <dasokol@umich.edu>
2024-02-24 08:02:37 -0500

    add anchor links to rendering page

--- src/pages/rendering.js
+++ src/pages/rendering.js
@@ -9,7 +9,7 @@ export default function Rendering() {
   const title = <Typography component="h1" variant="h4" align="center" fontWeight="600" gutterBottom>Rendering</Typography>
   const body = (
     <>
-      <Typography component="h2" variant="h6" align="left" fontWeight="600" paddingTop="24px" gutterBottom>What is rentaflop rendering?</Typography>      
+      <Typography id="what-is-rentaflop" component="h2" variant="h6" align="left" fontWeight="600" paddingTop="24px" gutterBottom>What is rentaflop rendering?</Typography>      
       <Typography align="left" paragraph>
 	Rentaflop is an on-demand cloud render farm. We leverage our global network of graphics cards to render 3D animations faster and cheaper for 3D artists. 
 	When 3D artists need to create an animation, instead of lowering the quality of their work to reduce render times or paying thousands to buy or rent the
@@ -17,13 +17,13 @@ export default function Rendering() {
 	We're so confident you'll enjoy using rentaflop that we'll give you your money back if you're not satisfied with the results.
       </Typography>
       
-      <Typography component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>Is it secure?</Typography>
+      <Typography id="security-and-privacy" component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>Is it secure?</Typography>
       <Typography align="left" paragraph>
 	Yes. Your work is encrypted upon upload and when placed on rendering machines. For customers with the most strict security and privacy requirements,
 	our <RentaflopLink href="/pricing" color="info.main">pro tier</RentaflopLink> limits rendering to only machines owned by rentaflop and trusted partners.
       </Typography>
 
-      <Typography component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>How much will it cost to render my project?</Typography>
+      <Typography id="pricing" component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>How much will it cost to render my project?</Typography>
       <Typography align="left" paragraph>
 	You can see exactly how much it'll cost ahead of time by <RentaflopLink href={portal_url+"/register"} color="info.main">creating an account</RentaflopLink> and
 	running our <RentaflopLink href={portal_url+"/launchpad"} color="info.main">price calculator</RentaflopLink>. Render your work for as low as 1/10th the price of other
@@ -31,7 +31,7 @@ export default function Rendering() {
 	See our <RentaflopLink href="/pricing" color="info.main">pricing</RentaflopLink> for more info.
       </Typography>
 
-      <Typography component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>How do I render my project?</Typography>
+      <Typography id="how-to-render" component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>How do I render my project?</Typography>
       <Typography align="left" paragraph>
 	The first step to rendering your project is to <RentaflopLink href={portal_url+"/register"} color="info.main">create an account</RentaflopLink>, if you haven't already.
 	You must also <RentaflopLink href={portal_url+"/payment"} color="info.main">add a payment method</RentaflopLink> prior to rendering. If you're not ready to add a payment
@@ -47,7 +47,7 @@ export default function Rendering() {
 	the <RentaflopLink href={portal_url+"/ground-control"} color="info.main">ground control</RentaflopLink> page.
       </Typography>
 
-      <Typography component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>How can I ensure my textures and other external files are included?</Typography>
+      <Typography id="including-external-files" component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>How can I ensure my textures and other external files are included?</Typography>
       <Typography align="left" paragraph>
 	Make sure to pack all necessary textures and external files into your file. In addition, we support .zip file uploads, so you may include assets, videos,
 	and caches from baking by zipping those with your file. For larger projects, we recommend running
@@ -57,7 +57,7 @@ export default function Rendering() {
 	Please configure your rendering settings (such as sample sizes, file output types, frames, etc.) to your liking from within your 3D application.
       </Typography>
 
-      <Typography component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>What rendering settings do you support?</Typography>
+      <Typography id="render-settings" component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>What rendering settings do you support?</Typography>
 	<Typography align="left" paragraph>
 	  We support the most recent version of Autodesk 3DS Max, Maya, and Maxon Cinema 4D with V-Ray and Arnold rendering engines.
 	We support rendering with both Cycles and Eevee, as well as most settings that come out-of-the-box with Blender. 
@@ -71,7 +71,7 @@ export default function Rendering() {
 	We support all built-in addons. If you're using 3rd party addons, or have additional
 	questions (or need different versions, larger file size, etc.) about rendering, please <RentaflopLink href={portal_url+"/contact"} color="info.main">contact us</RentaflopLink>.
 	</Typography>
-      <Typography component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>Can I render NSFW projects?</Typography>
+      <Typography id="nsfw-projects" component="h2" variant="h6" align="left" fontWeight="600" gutterBottom>Can I render NSFW projects?</Typography>
       <Typography align="left" paragraph>
 	Yes, you may render NSFW projects on rentaflop. However, you must not use rentaflop for any illegal purpose, including rendering projects containing
         illegal data.
